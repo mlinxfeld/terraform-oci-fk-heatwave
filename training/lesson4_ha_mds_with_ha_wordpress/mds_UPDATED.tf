a94a4c9cae9c1c9ba0ffbdbcf03816516e63cb73
mlinxfeld <martin.linxfeld@foggykitchen.com>
2024-08-26 15:52:07 +0000

    Lesson4

---
+++ training/lesson4_ha_mds_with_ha_wordpress/mds_UPDATED.tf
@@ -0,0 +1,14 @@
+module "oci-fk-mds" {
+  #source                                = "github.com/mlinxfeld/terraform-oci-fk-heatwave"
+  source                                = "../../"
+  mds_admin_password                    = var.mds_admin_password
+  mds_availability_domain               = var.mds_availability_domain
+  mds_compartment_ocid                  = var.mds_compartment_ocid
+  mds_shape                             = "MySQL.2"
+  mds_display_name                      = "FoggyKitchenMDSHA"
+  mds_description                       = "FoggyKitchen High Available MySQL/Heatwave Database System"
+  use_existing_vcn                      = true
+  subnet_id                             = oci_core_subnet.FoggyKitchenPrivateSubnet.id
+  mds_is_highly_available               = var.mds_is_highly_available
+}
+
